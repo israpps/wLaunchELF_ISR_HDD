095f3241daf61b436e7f542d3e4b18280313b816
israpps <isr_apps_help@hotmail.com>
2021-06-01 11:03:20 -0300

    add support for other extensions on text editor shortcut

--- filer.c
+++ filer.c
@@ -3857,9 +3857,17 @@ int getFilePath(char *out, int cnfmode)
 						iconcolr = COLOR_GRAPH1;
 					} else {
 						iconbase = ICON_FILE;
-						if (genCmpFileExt(files[top + i].name, "ELF"))
+						if (genCmpFileExt(files[top + i].name, "ELF") || genCmpFileExt(files[top + i].name, "KELF"))
 							iconcolr = COLOR_GRAPH2;
-						else if (genCmpFileExt(files[top + i].name, "TXT") || genCmpFileExt(files[top + i].name, "JPG") || genCmpFileExt(files[top + i].name, "JPEG"))
+						else if (
+									genCmpFileExt(files[top + i].name, "TXT") || 
+									genCmpFileExt(files[top + i].name, "CFG") || 
+									genCmpFileExt(files[top + i].name, "CNF") || 
+									genCmpFileExt(files[top + i].name, "INI") || 
+									genCmpFileExt(files[top + i].name, "CHT") || 
+									genCmpFileExt(files[top + i].name, "JPG") || 
+									genCmpFileExt(files[top + i].name, "JPEG")
+									)
 							iconcolr = COLOR_GRAPH4;
 						else
 							iconcolr = COLOR_GRAPH3;
